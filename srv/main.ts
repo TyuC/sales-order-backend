0e483934521fe0325319a3c00200b942d765fcf5
Luccas <luccasgoncalveslima@gmail.com>
2025-10-24 16:05:22 -0300

    feat: add main import Customer and Customers

---
+++ srv/main.ts
@@ -0,0 +1,10 @@
+import { Customer, Customers } from "@models/sales";
+
+const customer: Customer = {
+    email: 'teste@teste.com',
+    firstName: 'Luccas',
+    lastName: 'Lima',
+    id: '1234'
+};
+
+const customers: Customers = [customer];
