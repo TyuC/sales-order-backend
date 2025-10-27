f3328905176f769d58f9d3448af35809ad72e4e9
Luccas <luccasgoncalveslima@gmail.com>
2025-10-27 16:35:50 -0300

    feat: add products validation

--- srv/main.ts
+++ srv/main.ts
@@ -1,5 +1,5 @@
-import { Service } from '@sap/cds';
-import { Customers } from '@models/sales';
+import cds, { db, Request, Service } from '@sap/cds';
+import { Customers, Products, SalesOrderItem, SalesOrderItems } from '@models/sales';
 
 export default (service: Service) => {
     service.after('READ', 'Custumers', (results: Customers) => {
@@ -8,5 +8,32 @@ export default (service: Service) => {
                 customer.email = `${customer.email}@gmail.com`;
             }
         })
-    })
+    });
+    service.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
+        const params = request.data;
+        const items: SalesOrderItems = params.items;
+        if(params.customer_id){
+            return request.reject(400, 'Customer inválido');
+        }
+        if (params.items || params.items?.length === 0) {
+            return request.reject(400, 'Itens inválidos');
+        }
+        const customerQuery = SELECT.one.from('sales.Customers').where({ id: params.customer_id });
+        const customer = await cds.run(customerQuery);
+        if (!customer) {
+            return request.reject(404, 'Customer não encontrado');
+        }
+        const productsIds: string[] = params.items.map((item: SalesOrderItem) => item.product_id);
+        const productsQuery = SELECT.from('sales.Products').where({id: productsIds});
+        const products: Products = await cds.run(productsQuery);
+        for (const item of items) {
+            const dbProduct = products.find(product => product.id === item.product_id);
+            if (!dbProduct){
+                return request.reject(404, `Produto ${item.product_id} não encontrado`);
+            }
+            if (dbProduct.stock === 0) {
+                return request.reject(400, `Produto ${dbProduct.name}(${dbProduct.id}) sem estoque disponível`);
+            }
+        }
+    });
 }
